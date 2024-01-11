b4d9ed306a8081a5ed1f74a4264ead1210e8586a
smoljankin <smoljankin@gmail.com>
2024-01-11 14:01:14 +0200

    Added a second cube

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -37,6 +37,25 @@ class MainActivity : AppCompatActivity() {
         diceImage.setImageResource(drawableResource)
         // Update the content description
         diceImage.contentDescription = diceRoll.toString()
+
+        // Create new Dice object with 6 sides and roll the dice
+        val dice2 = Dice(6)
+        val diceRoll2 = dice2.roll()
+        // Find the ImageView in the layout
+        val diceImage2: ImageView = findViewById(R.id.imageView4)
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // Update the ImageView with the correct drawable resource ID
+        diceImage2.setImageResource(drawableResource2)
+        // Update the content description
+        diceImage2.contentDescription = diceRoll2.toString()
     }
 }
 
